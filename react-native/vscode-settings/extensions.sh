03fa8e1b280568e428376f8c6738f3339f016961
Louis Daily <developer@louisdaily.com>
2024-08-24 16:08:21 -0500

    feat(vscode): add extensions script
    Add script to install recommended VS Code extensions

---
+++ react-native/vscode-settings/extensions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension redhat.java
+code --install-extension vscjava.vscode-java-pack
+code --install-extension ms-vsliveshare.vsliveshare
